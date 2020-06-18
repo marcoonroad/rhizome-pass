6851560827f7347b5765c6637c83aa2f95624ea6
Marco Aurélio da Silva <marcoonroad@gmail.com>
2020-06-18 06:23:58 -0300

    Updates

---
+++ precache-manifest.f874c1eb0948c649e1b0592337dd3bc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8a9857e768e633c285535b86c6c383",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91ebf5307b6d055dc51e",
+    "url": "/static/css/main.d93fe625.chunk.css"
+  },
+  {
+    "revision": "497d4e10cb474b3d9760",
+    "url": "/static/js/2.9deec18e.chunk.js"
+  },
+  {
+    "revision": "91ebf5307b6d055dc51e",
+    "url": "/static/js/main.92c0ad17.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "185384526b28d13251b88395291154ef",
+    "url": "/static/media/stop.18538452.png"
+  }
+]);
